793d164afad28c32244b58494b3b1206cb4f55a7
mibichoandafugao <augustohenao1234@gmail.com>
2025-09-07 16:11:35 -0500

    Organizacion en los archivos

--- src/firebase/firabaseConfig.js
+++ src/firebase/firabaseConfig.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 
 // TODO: Add SDKs for Firebase products that you want to use
 const firebaseConfig = {
-  apiKey: "AIzaSyAH87ulQrRi-NAAsp2ZoIb1GB3Wvc6DsQs",
-  authDomain: "dbingeconomica.firebaseapp.com",
-  projectId: "dbingeconomica",
-  storageBucket: "dbingeconomica.firebasestorage.app",
-  messagingSenderId: "621733072241",
-  appId: "1:621733072241:web:2cb3a27fd995a44f18bad0",
-  measurementId: "G-XGEQ214HBS"
-};
+  apiKey: 'AIzaSyAH87ulQrRi-NAAsp2ZoIb1GB3Wvc6DsQs',
+  authDomain: 'dbingeconomica.firebaseapp.com',
+  projectId: 'dbingeconomica',
+  storageBucket: 'dbingeconomica.firebasestorage.app',
+  messagingSenderId: '621733072241',
+  appId: '1:621733072241:web:2cb3a27fd995a44f18bad0',
+  measurementId: 'G-XGEQ214HBS',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 
-export const auth = getAuth(app); 
+export const auth = getAuth(app)
---
+++ src/lib/data.js
@@ -0,0 +1,27 @@
+// Este archivo sirve para organizar las operaciones matematicas en un solo lugar, y no tenerlas
+// dentro de los mismos componentes
+
+export function calcularInteres() {
+  // Para calcular el interes tuve que recurrir a buscar en deepseek xd
+  // la formula que voy a usar es la de la tasa efectiva anual (TEA)
+  // que nos dice cual es el porcentaje de crecimiento real de nuestro dinero en un periodo
+  // de un año, teniendo en cuenta la capitalizacion
+  // TEA = (1 + (r/n))^n - 1
+  // TEA = tasa efectiva anual (expresada en decimal)
+  // r = tasa de interes nominal anual (la que anuncia el banco, también en decimal)
+  // n = numero de periodos (mensual=12, trimestral=4, diario=365)
+}
+
+// -----------------------------------
+
+export function calcularInteresSimple() {}
+
+// -----------------------------------
+
+export function calcularInteresCompuesto() {}
+
+// -----------------------------------
+
+export function calcularAnualidad() {}
+
+// -----------------------------------
