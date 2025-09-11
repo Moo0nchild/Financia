284e326eb87aded0cd0e5766610a46b8d456933d
Luis301311 <luisveal301@gmail.com>
2025-09-11 16:04:21 -0500

    Funcionalidad de Login, creacion de HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import Register from './pages/Register'
 import { Navigate } from 'react-router-dom'
 import { Interes } from './pages/Interes'
 import { InteresCompuesto } from './pages/interesCompuesto'
+import { Anualidades } from './pages/Anualidades'
+import Home from './pages/HomePage'
 export default function App() {
   return (
     <Routes>
-      <Route path='/' element={<Navigate to='/interesCompuesto' />} />
+      <Route path='/' element={<Navigate to='/login' />} />
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
       <Route path='/interes' element={<Interes />} />
       <Route path='/interesCompuesto' element={<InteresCompuesto />} />
+      <Route path='/anualidades' element={<Anualidades />} />
+      <Route path='/home' element={<Home />} />
     </Routes>
   )
 }
